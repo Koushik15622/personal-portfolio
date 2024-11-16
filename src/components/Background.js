9c650d11efc6e7c87feb6fbbda7a7dded86f640c
Koushik15622 <Koushikpln02@gmail.com>
2024-11-16 17:28:19 -0500

    first commit

---
+++ src/components/Background.js
@@ -0,0 +1,154 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import comp1 from "../assets/img/comp1.png";
+import comp2 from "../assets/img/comp2.png";
+import comp3 from "../assets/img/comp3.png";
+import comp4 from "../assets/img/comp4.png";
+import edu2 from "../assets/img/edu2.png";
+import iot from "../assets/img/iot.png";
+import python from "../assets/img/python.png";
+import mern from "../assets/img/mern.png";
+import java from "../assets/img/java.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Background = () => {
+
+  const companies = [
+    {
+      title: "Graduate Teaching Assistant",
+      description: "04/2024 - Present",
+      imgUrl: comp4,
+    },
+    {
+      title: "Software Developer",
+      description: "07/2023 - 11/2023",
+      imgUrl: comp3,
+    },
+    {
+      title: "Software Developer",
+      description: "06/2022 - 06/2023",
+      imgUrl: comp2,
+    },
+    {
+      title: "Associate Software Engineer",
+      description: "09/2021 - 06/2022",
+      imgUrl: comp1,
+    }
+  ];
+
+  const education = [
+    {
+      title: "Master's in Cybersecurity",
+      description: "Graduated with a CGPA of 3.9/4.0",
+      imgUrl: comp4,
+    },
+    {
+      title: "Bachelor's in Computer Science & Engineering",
+      description: "Graduated with a CGPA of 3.8/4.0",
+      imgUrl: edu2,
+    }
+  ];
+
+  const projects = [
+    {
+      title: "Research Initiative",
+      description: "Blockchain security system in IoT devices",
+      imgUrl: iot,
+    },
+    {
+      title: "Ongoing Research Project",
+      description: "RAG model to analyze of CISO Interviews",
+      imgUrl: python,
+    },
+    {
+      title: "Individual Project",
+      description: "Ecommerce Platform with Multi-User Support",
+      imgUrl: mern,
+    },
+    {
+      title: "Freelance Development Project",
+      description: "AUTO-MCP IDE for IC-processor Programming",
+      imgUrl: java,
+    }
+  ];
+
+
+  return (
+    <section className="project" id="background">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Professional Background</h2>
+                  <br></br>
+                  <br></br>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Work Experience</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Academic Journey</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Projects & Research</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            companies.map((company, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...company}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {
+                            education.map((edu, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...edu}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
---
+++ src/components/Newsletter.js
@@ -0,0 +1,53 @@
+import { Col, Row } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
+export const Newsletter = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      // Fetch the file from the server
+      let response = await fetch("http://localhost:5000/", {
+        method: "GET",
+      });
+  
+      // Check if the response is OK
+      if (response.ok) {
+        // Convert the response to a Blob
+        const blob = await response.blob();
+        // Create a download link
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.style.display = "none";
+        a.href = url;
+        a.download = "Koushik-P.pdf"; // The file name for the download
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url); // Clean up the URL object
+        console.log("File downloaded successfully");
+      } else {
+        console.log("Failed to download the file");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  
+    return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={10} xl={10}>
+            <h3>Interested in knowing more about my skills and experience?<br></br>You can download my resume here <ArrowRightCircle size={25} /></h3>
+            </Col>
+            <Col lg={12} md={2} xl={2}>
+              <form onSubmit={handleSubmit}>
+                  <button id="btn" type="submit">Download</button>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+    )
+  }
